853cba734ba9ffac1a91cb8c65da51f43d49d5f2
alexpaulo100 <alex.paulo100@gmail.com>
2022-01-02 19:37:54 -0300

    bootcamp e conteudo

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import br.com.desafio.dominio.Conteudo;
 import br.com.desafio.dominio.Curso;
 import br.com.desafio.dominio.Mentoria;
 
@@ -19,6 +20,8 @@ public class Main {
         mentoria.setTitulo("mentoria de java");
         mentoria.setDescricao("descricao mentoria java");
         mentoria.setData(LocalDate.now());
+
+        Conteudo conteudo = new Curso();
 
         System.out.println(curso1);
         System.out.println(curso2);
