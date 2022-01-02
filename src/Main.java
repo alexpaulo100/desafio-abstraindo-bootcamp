3b5ca457124cd90c2acd60ffad4ae3d8d92ad7cb
alexpaulo100 <alex.paulo100@gmail.com>
2022-01-02 18:46:48 -0300

    mentoria e curso

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import br.com.desafio.dominio.Curso;
+import br.com.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descricao curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso Python");
+        curso2.setDescricao("descricao curso python");
+        curso2.setCargaHoraria(10);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descricao mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+
+
+
+    }
+}
